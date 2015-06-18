d6c1a5fb94c5008d5a2c87268bc592af579d125c
Yuki Suga <ysuga@ysuga.net>
2015-06-18 10:17:22 +0900

    update onexecute

--- src/SFMLJoystickToVelocity.cpp
+++ src/SFMLJoystickToVelocity.cpp
@@ -26,9 +26,10 @@ static const char* sfmljoysticktovelocity_spec[] =
     "lang_type",         "compile",
     // Configuration variables
     "conf.default.debug", "0",
-    "conf.default.gainX", "0.01",
-    "conf.default.gainTh", "0.01",
-    "conf.default.xIndex", "0",
+    "conf.default.gainX", "-0.005",
+    "conf.default.gainTh", "-0.005",
+    "conf.default.xIndex", "1",
+    "conf.default.thIndex", "0",
     // Widget
     "conf.__widget__.debug", "text",
     "conf.__widget__.gainX", "text",
@@ -86,6 +87,7 @@ RTC::ReturnCode_t SFMLJoystickToVelocity::onInitialize()
   bindParameter("gainX", m_gainX, "0.01");
   bindParameter("gainTh", m_gainTh, "0.01");
   bindParameter("xIndex", m_xIndex, "0");
+  bindParameter("thIndex", m_thIndex, "1");
   // </rtc-template>
   
   return RTC::RTC_OK;
@@ -121,12 +123,34 @@ RTC::ReturnCode_t SFMLJoystickToVelocity::onActivated(RTC::UniqueId ec_id)
 
 RTC::ReturnCode_t SFMLJoystickToVelocity::onDeactivated(RTC::UniqueId ec_id)
 {
+	m_velocity.data.vx = 0;
+	m_velocity.data.vy = 0;
+	m_velocity.data.va = 0;
+	m_velocityOut.write();
   return RTC::RTC_OK;
 }
 
 
 RTC::ReturnCode_t SFMLJoystickToVelocity::onExecute(RTC::UniqueId ec_id)
 {
+	if(m_inIn.isNew()) {
+		m_inIn.read();
+
+		if(m_in.data.length() <= m_xIndex || m_in.data.length() <= m_thIndex) {
+			std::cout << "[RTC::SFMLJoystickToVelocity] Too short Sequence Input." << std::endl;
+			return RTC::RTC_ERROR;
+		}
+
+		m_velocity.data.vx = m_gainX * m_in.data[m_xIndex];
+		m_velocity.data.vy = 0;
+		m_velocity.data.va = m_gainTh * m_in.data[m_thIndex];
+		m_velocityOut.write();
+
+		if(m_debug) {
+			std::cout << "[RTC::SFMLJoystickToVelocity] Velocity = " << m_velocity.data.vx << ", " << m_velocity.data.vy << ", " << m_velocity.data.va << std::endl;
+		}
+	}
+
   return RTC::RTC_OK;
 }
 
